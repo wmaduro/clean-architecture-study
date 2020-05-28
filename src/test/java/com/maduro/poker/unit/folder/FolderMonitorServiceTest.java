ad5c82211c05915d9102fb4ced14e7c7ad04bf22
welerson maduro <wmaduro@gmail.com>
2020-05-29 11:46:40 +1200

    FolderMonitorServiceTest - Awaiting

--- src/test/java/com/maduro/poker/unit/folder/FolderMonitorServiceTest.java
+++ src/test/java/com/maduro/poker/unit/folder/FolderMonitorServiceTest.java
@@ -4,9 +4,12 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.Duration;
 import java.util.Arrays;
+import java.util.concurrent.Callable;
 import java.util.concurrent.Executors;
 
+import org.awaitility.Awaitility;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
@@ -26,26 +29,23 @@ class FolderMonitorServiceTest {
 
 	@Test
 	void mustRenameCSVFiles() throws IOException {
-
+		
+		final int TIMEOUT_TWO_SEC = 2;
+		
 		File csvFile = FolderMonitorServiceUtils.createImportFile(tempDir, this.getClass().getCanonicalName());
-
-		try {
-			Thread.sleep(500);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+		String importFileName = Utils.removeFileExtension(csvFile.getAbsoluteFile().toString())
+				+ FolderMonitorService.IMPORTING_EXTENSION;
 
 		FolderMonitorService folderMonitorService = new FolderMonitorService(null, tempDir.toPath());
-		Executors.newCachedThreadPool().submit(folderMonitorService);
+		Executors.newCachedThreadPool().execute(folderMonitorService);
 
-		try {
-			Thread.sleep(500);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-		String importFileName = csvFile.getAbsoluteFile().toString();
-		importFileName = Utils.removeFileExtension(importFileName) + FolderMonitorService.IMPORTING_EXTENSION;
+		final File importFile = new File(importFileName);
+		Awaitility.await().timeout(Duration.ofSeconds(TIMEOUT_TWO_SEC)).until(new Callable<Boolean>() {
+			@Override
+			public Boolean call() throws Exception {
+				return importFile.exists();
+			}
+		});
 
 		assertTrue(new File(importFileName).exists());
 
